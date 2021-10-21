2ac5aa54bb9557f1d30f325e31ae49c2113c51a3
Mustafa K. Waly <mustafa.kamel@estafsar.com>
2021-10-22 00:57:39 +0200

    Fix var is not present

--- resources/views/admin/products/form.blade.php
+++ resources/views/admin/products/form.blade.php
@@ -6,9 +6,11 @@
 </div>
 <div class="form-group {{ $errors->has('image') ? 'has-error' : ''}}">
     <label for="image" class="control-label">{{ 'Image' }}</label>
+    @isset($product)
     @if($product->image)
     <img style="width:20%" src={{ asset($product->image) }}>
     @endif
+    @endisset
     <input class="form-control" name="image" type="file" id="image"
         value="{{ isset($product->image) ? $product->image : ''}}">
     {!! $errors->first('image', '<p class="help-block">:message</p>') !!}
